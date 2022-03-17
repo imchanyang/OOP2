da7318821fd0a279e77657bc7cbe76e71f61c5fd
imchanyang <cksdid636@naver.com>
2022-03-17 13:57:55 +0900

    const change error

---
+++ Week2/Week2-3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.14159;
+
+    double radius;
+    double perimeter;
+    double area;
+
+    cout << "원의 반지름 입력: ";
+    cin >> radius;
+
+    perimeter = 2 * PI * radius;
+    area = PI * radius * radius;
+
+    cout << "반지름: " << radius << endl;
+    cout << "둘레: " << perimeter << endl;
+    cout << "면적 : " << area << endl;
+
+    PI = 3.141592;
+
+    return 0;
+}
