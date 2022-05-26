24d432a40d7edf72e42dd4413be30291ece0fae1
imchanyang <cksdid636@naver.com>
2022-05-26 11:41:19 +0900

    12-2

---
+++ Week13/12-2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Base{
+public:
+    virtual void print () const {cout << "베이스 클래스" << endl;}
+};
+
+class Derived : public Base{
+public:
+    virtual void print () const {cout << "파생 클래스" << endl;}
+};
+
+int main ( ){
+
+    Base* ptr;
+
+    ptr = new Base ();
+    ptr -> print();
+    delete ptr;
+
+    ptr = new Derived();
+    ptr -> print();
+    delete ptr;
+    return 0;
+}
