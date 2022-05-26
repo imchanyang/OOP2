46e256e399827c4c029692a6a9e3f2af2ff955fc
imchanyang <cksdid636@naver.com>
2022-05-26 11:34:26 +0900

    11-24~11-28

---
+++ Week13/11-24~11-28/invoice.cpp
@@ -0,0 +1,17 @@
+#include "invoice.h"
+
+Invoice :: Invoice (int invNum)
+: invoiceNumber (invNum), invoiceTotal (0.0){
+}
+
+Invoice :: ~Invoice ( ){
+}
+
+void Invoice :: add (int quantity, Product product){
+    invoiceTotal += quantity * product.getPrice ();
+}
+
+void Invoice :: print ( ) const{
+    cout << "청구 번호: "<< invoiceNumber << endl;
+    cout << "청구 금액: " << invoiceTotal << endl;
+}
