b1859d80dc16352f446efc75f4ef53b77678cd46
imchanyang <cksdid636@naver.com>
2022-05-27 10:42:59 +0900

    12-10~12-21

---
+++ Week13/12-10~12-21/rectangle.cpp
@@ -0,0 +1,28 @@
+#include "rectangle.h"
+// Constructor
+Rectangle :: Rectangle (double lg, double wd)
+: length (lg), width (wd){
+    if (!isValid()){
+        cout << "유효하지않은 직사각형입니다.";
+        assert (false);
+    }
+}
+
+Rectangle :: ~Rectangle (){
+}
+
+void Rectangle :: print () const{
+    cout << "직사각형의 크기=  " << length << " X " << width << endl;
+}
+
+double Rectangle :: getArea() const{
+    return length * width;
+}
+
+double Rectangle :: getPerimeter() const{
+    return 2 * (length + width);
+}
+
+bool Rectangle :: isValid () const{
+    return (length > 0.0 && width > 0.0);
+}
---
+++ Week13/12-10~12-21/shape.cpp
@@ -0,0 +1,5 @@
+//
+// Created by imchanyang on 2022/05/27.
+//
+
+#include "shape.h"
